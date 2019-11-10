17b61823ebb011ce89091f34b5906992004e7788
Timothy Morton <tim.morton@gmail.com>
2019-11-09 16:05:27 -0800

    add basic lensing lite lhood

---
+++ cobaya/lensing_lite.py
@@ -0,0 +1,33 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+from scipy.stats import binned_statistic as binnedstat
+
+def binner(ls, cls, bin_edges):
+    x = ls.copy()
+    y = cls.copy()
+    numbins = len(bin_edges)-1
+    cents = (bin_edges[:-1]+bin_edges[1:])/2.
+    bin_edges_min = bin_edges.min()
+    bin_edges_max = bin_edges.max()
+    y[x<bin_edges_min] = 0
+    y[x>bin_edges_max] = 0
+    bin_means = binnedstat(x,y,bins=bin_edges,statistic=np.nanmean)[0]
+    return cents, bin_means
+
+datafile = 'simulated_clkk_SO_Apr17_mv_nlkk_deproj0_SENS1_fsky_16000_iterOn_20191109/simulated_clkk_SO_Apr17_mv_nlkk_deproj0_SENS1_fsky_16000_iterOn_20191109_sim_00_bandpowers.txt'
+covfile = 'simulated_clkk_SO_Apr17_mv_nlkk_deproj0_SENS1_fsky_16000_iterOn_20191109/simulated_clkk_SO_Apr17_mv_nlkk_deproj0_SENS1_fsky_16000_iterOn_20191109_binned_covmat.txt'
+lmax = 2400
+
+class LensingLiteLikelihood(object):
+    def __init__(self, bandpower_file=datafile, covfile=covfile):
+        lefts, rights, bandpowers = np.loadtxt(bandpower_file, unpack=True)
+        self.bandpowers = bandpowers
+        self.bin_edges = np.append(lefts, [rights[-1]])
+        self.cov = np.loadtxt(covfile)
+        
+    def __call__(self, _theory={'Cl': {'tt': lmax, 'pp': lmax}}):
+        cl = _theory.get_Cl(ell_factor=True)
+        _, theory_pp = binner(cl['ell'], cl['pp'], self.bin_edges)
+
+        return multivariate_normal.logpdf(self.bandpowers, mean=theory_pp, cov=self.cov)        
+    
