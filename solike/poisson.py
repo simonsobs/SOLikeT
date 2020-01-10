def027616ea306c48097d9ec04c3cf707ca04444
Nicholas Battaglia <nbatta@astro.cornell.edu>
2020-01-10 14:26:22 -0500

    started editiing clusterlike

--- solike/poisson.py
+++ solike/poisson.py
@@ -1,12 +1,12 @@
-import pandas as pd
+import .survey as Survey
 
 from cobaya.likelihood import Likelihood
 
 from .poisson_data import PoissonData
-
+import .massfunc as mf
 
 class PoissonLikelihood(Likelihood):
-    class_options = {"name": "Poisson", "catalog_path": None, "columns": None}
+    class_options = {"name": "Cluster", "data_path": None, "data_name": None, "columns": None}
 
     def initialize(self):
         catalog = self._get_catalog()
@@ -14,8 +14,15 @@ class PoissonLikelihood(Likelihood):
             self.columns = catalog.columns
         self.data = PoissonData(self.name, catalog, self.columns)
 
+    def get_requirements(self):
+        return {'Pk_interpolator':{'z': np.linspace(0,2,41), 'k_max': 5.0, 
+                                   'nonlinear': False,'hubble_units': True,'k_hunit': True, 
+                                   'vars_pairs': [['delta_nonu','delta_nonu']]},
+                'H':{'z': np.linspace(0,2,41)}}
+
     def _get_catalog(self):
-        return pd.read_csv(self.catalog_path)
+        catalog = Survey.SurveyData(self.data_path, self.data_name)
+        return catalog
 
     def _get_rate_fn(self, **kwargs):
         """Returns a callable rate function that takes each of 'columns' as kwargs.
