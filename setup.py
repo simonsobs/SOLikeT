9ac5a45e9524f7bbd8ab9ea700a3032adbb40139
Tim Morton <tim.morton@gmail.com>
2021-02-03 12:30:46 -0800

    pyccl not ccl

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "scikit-learn",
         "cobaya",
         "sacc",
-        "ccl",
+        "pyccl",
         "fgspectra @ git+https://github.com/simonsobs/fgspectra@master#egg=fgspectra",
         "mflike @ git+https://github.com/simonsobs/lat_mflike"
     ],
