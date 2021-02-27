50609e11b3faa617cf188acc669d38380b998cca
Boris Bolliet <boris.bolliet@gmail.com>
2021-02-26 22:44:58 -0500

    Update install_class_sz_linux.sh

--- ci_scripts/install_class_sz_linux.sh
+++ ci_scripts/install_class_sz_linux.sh
@@ -4,6 +4,13 @@ sudo apt-get install libgsl-dev
 dpkg -L libgsl-dev
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/share/miniconda/envs/test/lib/
 export LD_LIBRARY_PATH
+cd /home/runner/work/SOLikeT/SOLikeT/
+wget ftp://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz
+tar -zxvf gsl-2.6.tar.gz
+cd gsl-2.6
+./configure --prefix=/home/runner/work/SOLikeT/SOLikeT/gsl-2.6
+make
+make install
 git clone https://github.com/borisbolliet/class_sz.git
 cd class_sz
 export DYLD_PRINT_LIBRARIES=1
