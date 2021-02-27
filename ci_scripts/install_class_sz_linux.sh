799697985f875890d2ee3599e7d89b8744362660
Boris Bolliet <boris.bolliet@gmail.com>
2021-02-26 19:57:15 -0500

    Update install_class_sz_linux.sh

--- ci_scripts/install_class_sz_linux.sh
+++ ci_scripts/install_class_sz_linux.sh
@@ -2,6 +2,8 @@
 
 sudo apt-get install libgsl-dev
 dpkg -L libgsl-dev
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/
+export LD_LIBRARY_PATH
 git clone https://github.com/borisbolliet/class_sz.git
 cd class_sz
 make -j4
