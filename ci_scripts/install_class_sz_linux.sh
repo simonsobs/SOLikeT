9584c3499a71a768d4638c858b81b779a1182adf
Boris Bolliet <boris.bolliet@gmail.com>
2021-02-26 21:49:25 -0500

    Update install_class_sz_linux.sh

--- ci_scripts/install_class_sz_linux.sh
+++ ci_scripts/install_class_sz_linux.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install libgsl-dev
 dpkg -L libgsl-dev
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/share/miniconda/envs/test/lib/
 export LD_LIBRARY_PATH
 git clone https://github.com/borisbolliet/class_sz.git
 cd class_sz
