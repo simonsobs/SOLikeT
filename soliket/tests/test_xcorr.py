checks against CCL, fixed units in calculation of mag kernel

soliket/tests/test_xcorr.py
@@ -7,6 +7,7 @@ from cobaya.yaml import yaml_load
 from cobaya.model import get_model
 
 import os
+import pdb
 
 def get_demo_xcorr_model(theory):
     if theory == "camb":
@@ -18,15 +19,20 @@ def get_demo_xcorr_model(theory):
         theory:
             camb:
                 extra_args:
-                    lens_potential_accuracy: 1
+                  lens_potential_accuracy: 1
 
         params:
+            tau: 0.05
+            mnu: 0.0
+            nnu: 3.046
             b1: 
                 prior:
                     min: 0.
                     max: 10.
             s1: 
-                value: 0.4
+                prior:
+                    min: 0.1
+                    max: 1.0
         """
     elif theory == "classy":
         info_yaml = r"""
@@ -46,7 +52,9 @@ def get_demo_xcorr_model(theory):
                     min: 0.
                     max: 10.
             s1: 
-                value: 0.4
+                prior:
+                    min: 0.1
+                    max: 1.0
 
         """
 
@@ -58,7 +66,7 @@ def get_demo_xcorr_model(theory):
 @pytest.mark.parametrize("theory", ["camb"])#, "classy"])
 def test_xcorr(theory):
 
-    params = {"b1": 1.0, "s1": 0.4}
+    params = {'b1': 1.0, 's1': 0.5}
 
     model = get_demo_xcorr_model(theory)
     lnl = model.loglike(params)[0]
@@ -95,26 +103,41 @@ def test_xcorr(theory):
     from matplotlib import pyplot as plt
     test_dir = os.path.dirname(os.path.abspath(__file__))
 
-    # c_ells = xcorr_lhood.theory.get_Cl(ell_factor=False)
-    # Cl_theo = c_ells["pp"][0 : xcorr_lhood.high_ell+1]
-    # Clkk_theo = (xcorr_lhood.ell_range * (xcorr_lhood.ell_range + 1)) ** 2 * Cl_theo * 0.25
+    import pyccl as ccl
+    cosmo = ccl.Cosmology(Omega_c=xcorr_lhood.provider.get_param('omch2') / (xcorr_lhood.provider.get_param('H0') / 100 * xcorr_lhood.provider.get_param('H0') / 100),
+                          Omega_b=xcorr_lhood.provider.get_param('ombh2') / (xcorr_lhood.provider.get_param('H0') / 100 * xcorr_lhood.provider.get_param('H0') / 100),
+                          h=xcorr_lhood.provider.get_param('H0') / 100,
+                          n_s=xcorr_lhood.provider.get_param('ns'),
+                          A_s=xcorr_lhood.provider.get_param('As'),
+                          Omega_k=xcorr_lhood.provider.get_param('omk'),
+                          Neff=xcorr_lhood.provider.get_param('nnu'),
+                          matter_power_spectrum='linear')
 
-    N_ell_auto = xcorr_lhood.ell_auto.shape[0]
-    N_ell_cross = xcorr_lhood.ell_cross.shape[0]
+    tracer_g = ccl.NumberCountsTracer(cosmo,
+                                      has_rsd=False,
+                                      dndz = xcorr_lhood.dndz.T,
+                                      bias =(xcorr_lhood.dndz[:,0], params['b1']*np.ones(len(xcorr_lhood.dndz[:,0]))), 
+                                      mag_bias = (xcorr_lhood.dndz[:,0], params['s1']*np.ones(len(xcorr_lhood.dndz[:,0])))
+                                      )
+
+    tracer_k = ccl.CMBLensingTracer(cosmo, z_source=1100)
+
+    cl_gg_ccl = ccl.cls.angular_cl(cosmo, tracer_g, tracer_g, xcorr_lhood.ell_range)
+    cl_kappag_ccl = ccl.cls.angular_cl(cosmo, tracer_k, tracer_g, xcorr_lhood.ell_range)
 
     plt.close('all')
     plt.figure(1, figsize=(2*4.5, 3.75))
     plt.subplot(121)
-    plt.plot(xcorr_lhood.ell_range, 1.e5*cl_gg, '--', color='C1', label='soliket.xcorr')
-    plt.plot(xcorr_lhood.data.x[:N_ell_auto], 1.e5*xcorr_lhood.data.y[:N_ell_auto], 'o', color='C2', label='simonsobs/xcorr')
+    plt.plot(xcorr_lhood.ell_range, 1.e5*cl_gg_ccl, '+', color='C2', label='CCL (soliket.cross_correlation)')
+    plt.plot(xcorr_lhood.ell_range, 1.e5*cl_gg, '-', color='C1', label='soliket.xcorr')
     plt.xlabel('$\\ell$')
     plt.ylabel('$C_{\\ell}$')
     plt.xlim([0,600])
     plt.title('$gg$')
     plt.legend(loc='upper right', fontsize='small')
     plt.subplot(122)
-    plt.plot(xcorr_lhood.ell_range, xcorr_lhood.ell_range*1.e5*cl_kappag, '--', color='C1')
-    plt.plot(xcorr_lhood.data.x[:-N_ell_cross], xcorr_lhood.ell_cross*1.e5*xcorr_lhood.data.y[:-N_ell_cross], 'o', color='C2')
+    plt.plot(xcorr_lhood.ell_range, xcorr_lhood.ell_range*1.e5*cl_kappag_ccl, '+', color='C2')
+    plt.plot(xcorr_lhood.ell_range, xcorr_lhood.ell_range*1.e5*cl_kappag, '-', color='C1')
     plt.ylabel('$\\ell C_{\\ell}$')
     plt.xlabel('$\\ell$')
     plt.xlim([0,600])

soliket/xcorr/limber.py
@@ -33,11 +33,13 @@ def mag_bias_kernel(cosmo, dndz, s1, zatchi, chi_arr, chiprime_arr, zprime_arr):
         norm = np.trapz(dndz[:,1], x=dndz[:,0])
         dndzprime  = dndzprime/norm #TODO check this norm is right
 
-        g_integrand = (chiprime_arr - chi_arr[np.newaxis,:]) / chiprime_arr * np.sqrt(cosmo.get_param('omegam') * (1 + zprime_arr)**3. + 1 - cosmo.get_param('omegam') ) * dndzprime
+        g_integrand = (chiprime_arr - chi_arr[np.newaxis,:]) / chiprime_arr \
+                        * (oneover_chmpc * cosmo.get_param('H0') / 100) * np.sqrt(cosmo.get_param('omegam') * (1 + zprime_arr)**3. + 1 - cosmo.get_param('omegam') ) \
+                        * dndzprime
 
         g = chi_arr * np.trapz(g_integrand, x=chiprime_arr, axis=0)
 
-        W_mu = (5. * s1 - 2.) * 1.5 * cosmo.get_param('omegam') * (oneover_chmpc)**2 * (1. + zatchi(chi_arr)) * g
+        W_mu = (5. * s1 - 2.) * 1.5 * cosmo.get_param('omegam') * (cosmo.get_param('H0') / 100)**2 * (oneover_chmpc)**2 * (1. + zatchi(chi_arr)) * g
 
         return W_mu
 
@@ -47,13 +49,13 @@ def do_limber(ell_arr, cosmo, dndz1, dndz2, s1, s2, pk, b1_HF, b2_HF, alpha_auto
     chistar = cosmo.get_comoving_radial_distance(cosmo.get_param('zstar'))
 
     # Galaxy kernels, assumed to be b(z) * dN/dz
-    W_g1    = np.interp(zatchi(chi_arr), dndz1[:,0], dndz1[:,1]*cosmo.get_Hubble(dndz1[:,0], units='1/Mpc'), left=0, right=0)
+    W_g1    = np.interp(zatchi(chi_arr), dndz1[:,0], dndz1[:,1] * cosmo.get_Hubble(dndz1[:,0], units='1/Mpc'), left=0, right=0)
     if not normed:
-        W_g1   /= np.trapz(W_g1,x=chi_arr)
+        W_g1   /= np.trapz(W_g1, x=chi_arr)
 
-    W_g2    = np.interp(zatchi(chi_arr), dndz2[:,0], dndz2[:,1]*cosmo.get_Hubble(dndz2[:,0], units='1/Mpc'), left=0, right=0)
+    W_g2    = np.interp(zatchi(chi_arr), dndz2[:,0], dndz2[:,1] * cosmo.get_Hubble(dndz2[:,0], units='1/Mpc'), left=0, right=0)
     if not normed:
-        W_g2   /= np.trapz(W_g2,x=chi_arr)
+        W_g2   /= np.trapz(W_g2, x=chi_arr)
 
     W_kappa = (oneover_chmpc)**2. * 1.5 * cosmo.get_param('omegam') * (cosmo.get_param('H0') / 100)**2. \
                 * (1. + zatchi(chi_arr)) * chi_arr * (chistar - chi_arr) / chistar
@@ -94,13 +96,13 @@ def do_limber(ell_arr, cosmo, dndz1, dndz2, s1, s2, pk, b1_HF, b2_HF, alpha_auto
         # W_kappakappa = W_kappa[i_chi] * W_kappa[i_chi] / (chi**2) * p_mm
         # c_ell_kappakappa[:,:,i_chi] = W_kappakappa.T
 
-        W_g1mu1 = W_g1[i_chi]*W_mu1[i_chi] / chi**2 * p_gm
+        W_g1mu1 = W_g1[i_chi]*W_mu1[i_chi] / (chi**2) * p_gm
         c_ell_g1mu1[:,:,i_chi] = W_g1mu1.T
 
-        W_mu1mu1 = W_mu1[i_chi]*W_mu1[i_chi] / chi**2 * p_mm
+        W_mu1mu1 = W_mu1[i_chi]*W_mu1[i_chi] / (chi**2) * p_mm
         c_ell_mu1mu1[:,:,i_chi] = W_mu1mu1.T
 
-        W_mu1kappa = W_kappa[i_chi]*W_mu1[i_chi] / chi**2 * p_mm
+        W_mu1kappa = W_kappa[i_chi]*W_mu1[i_chi] / (chi**2) * p_mm
         c_ell_mu1kappa[:,:,i_chi] = W_mu1kappa.T
 
 

soliket/xcorr/xcorr.py
@@ -20,12 +20,8 @@ class XcorrLikelihood(GaussianLikelihood):
         self.dndz = np.loadtxt(self.dndz_file)
 
         # TODO is this resolution limit on zarray a CAMB problem?
-        # TODO see also the zmax
-        # TODO allow calculation of chistar
-        #self.zarray = np.append(np.linspace(self.dndz[:,0].min(), self.dndz[:,0].max(), 149), [1200.]) #self.dndz[:,0]
-        self.zarray = np.linspace(self.dndz[:,0].min(), self.dndz[:,0].max(), 149) #self.dndz[:,0]
-        self.zbgdarray = np.concatenate([self.zarray, [1100.]])
-        #self.zbgdarray = np.linspace(self.dndz[:,0].min(), 1100, 149) #self.dndz[:,0]
+        self.zarray = np.linspace(self.dndz[:,0].min(), self.dndz[:,0].max(), 149)
+        self.zbgdarray = np.concatenate([self.zarray, [1100]]) # TODO: unfix zstar
         self.Nchi = 149
         self.Nchi_mag = 149
 