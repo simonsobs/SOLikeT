4d2bd56155a8bbf80b3a6c07290e9d7c6ff6fbaf
Ian Harrison <itrharrison@gmail.com>
2021-10-29 10:53:40 +0100

    linear bias model and testing

--- soliket/bias.py
+++ soliket/bias.py
@@ -6,16 +6,29 @@ import numpy as np
 from typing import Sequence, Union
 from cobaya.theory import Theory
 
+
 class Bias(Theory):
 
-    def initialize(self):
-        kmax: float = 0  # Maximum k (1/Mpc units) for Pk, or zero if not needed
-        nonlinear: bool = False  # whether to get non-linear Pk from CAMB/Class
-        z: Union[Sequence, np.ndarray] = []  # redshift sampling
+    kmax: float = 10.  # Maximum k (1/Mpc units) for Pk, or zero if not needed
+    nonlinear: bool = False  # whether to get non-linear Pk from CAMB/Class
+    z: Union[Sequence, np.ndarray] = []  # redshift sampling
 
-    # def get_requirements(self):
+    extra_args: dict = {}  # extra (non-parameter) arguments passed to ccl.Cosmology()
+
+    _logz = np.linspace(-3, np.log10(1100), 150)
+    _default_z_sampling = 10**_logz
+    _default_z_sampling[0] = 0
+
+    def initialize(self):
+
+        self._var_pairs = set()
+
+    def get_requirements(self):
+        return {}
 
     def must_provide(self, **requirements):
+
+        options = requirements.get('linear_bias') or {}
 
         self.kmax = max(self.kmax, options.get('kmax', self.kmax))
         self.z = np.unique(np.concatenate(
@@ -24,6 +37,11 @@ class Bias(Theory):
 
         # Dictionary of the things needed from CAMB/CLASS
         needs = {}
+
+        self.nonlinear = self.nonlinear or options.get('nonlinear', False)
+        self._var_pairs.update(
+            set((x, y) for x, y in
+                options.get('vars_pairs', [('delta_tot', 'delta_tot')])))
 
         needs['Pk_grid'] = {
                 'vars_pairs': self._var_pairs or [('delta_tot', 'delta_tot')],
@@ -34,23 +52,37 @@ class Bias(Theory):
 
         return needs
 
+    def _get_Pk_mm(self):
+
+        for pair in self._var_pairs:
+
+            if self.nonlinear:
+                k, z, Pk_nonlin = self.provider.get_Pk_grid(var_pair=pair,
+                                                            nonlinear=True)
+                Pk_mm = np.flip(Pk_nonlin, axis=0)
+            else:
+                k, z, Pk_lin = self.provider.get_Pk_grid(var_pair=pair,
+                                                         nonlinear=False)
+                Pk_mm = np.flip(Pk_lin, axis=0)
+
+        return Pk_mm
+
     # def calculate(self, state, want_derived=True, **params_values_dict):
-
-    # def get_Pkgg(self):
-
-    # def get_Pkgm(self):
-
-class Linear_bias(Bias):
-
-    params = {'b_lin' : None}
-
-    def calculate(self, state, want_derived=True, **params_values_dict):
-
-        state['Pk_gg'] = self.b_lin * self.b_lin * Pk_grid
-        state['Pk_gm'] = self.b_lin * Pk_grid
 
     def get_Pkgg(self):
         return self._current_state['Pk_gg']
 
     def get_Pkgm(self):
         return self._current_state['Pk_gm']
+
+
+class Linear_bias(Bias):
+
+    params = {'b_lin': None}
+
+    def calculate(self, state, want_derived=True, **params_values_dict):
+
+        Pk_mm = self._get_Pk_mm()
+
+        state['Pk_gg'] = params_values_dict['b_lin']**2. * Pk_mm
+        state['Pk_gm'] = params_values_dict['b_lin'] * Pk_mm
--- soliket/tests/test_bias.py
+++ soliket/tests/test_bias.py
@@ -4,26 +4,61 @@ import pytest
 import numpy as np
 
 from cobaya.model import get_model
+from cobaya.run import run
+
 
 def test_bias_import():
     from soliket.bias import Bias
 
+
 def test_linear_bias_import():
     from soliket.bias import Linear_bias
 
-def test_linear_bias():
+
+def test_linear_bias_model():
 
     from soliket.bias import Linear_bias
 
-    info = {"params": {"b_lin": 1.,
+    info = {"params": {
+                       "b_lin": 1.,
+                       "H0": 70.,
                        "ombh2": 0.0245,
                        "omch2": 0.1225,
-                       "tau": 0.05},
-            "likelihood": {"one" : None},
-            "theory": {#"camb" : None,
+                       "ns": 0.96,
+                       "As": 2.2e-9,
+                       "tau": 0.05
+                       },
+            "likelihood": {"one": None},
+            "theory": {"camb": None,
                        "linear_bias": {"external": Linear_bias}
                        },
-            "sampler": {"evaluate": None}
+            "sampler": {"evaluate": None},
+            "debug": True
            }
 
-    model = get_model(info)
+    model = get_model(info)  # noqa F841
+
+
+def test_linear_bias_run():
+
+    from soliket.bias import Linear_bias
+
+    info = {"params": {
+                       "b_lin": 1.,
+                       "H0": 70.,
+                       "ombh2": 0.0245,
+                       "omch2": 0.1225,
+                       "ns": 0.96,
+                       "As": 2.2e-9,
+                       "tau": 0.05
+                       },
+            "likelihood": {"one": None},
+            "theory": {"camb": None,
+                       "linear_bias": {"external": Linear_bias}
+                       },
+            "sampler": {"evaluate": None},
+            "debug": True
+           }
+
+    model = get_model(info)  # noqa F841
+    updated_info, sampler = run(info)
