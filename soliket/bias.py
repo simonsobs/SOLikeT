8896b5230c4d24eb78afd9633aa8d21155a933e6
Ian Harrison <itrharrison@gmail.com>
2021-11-01 18:12:10 +0000

    added interpolators (need tidying)

--- soliket/bias.py
+++ soliket/bias.py
@@ -6,6 +6,7 @@ import pdb
 import numpy as np
 from typing import Sequence, Union
 from cobaya.theory import Theory
+from cobaya.theories.cosmo.boltzmannbase import PowerSpectrumInterpolator
 
 
 class Bias(Theory):
@@ -52,7 +53,7 @@ class Bias(Theory):
                 'k_max': self.kmax
             }
 
-        assert len(self._var_pairs) < 2, "CCL doesn't support other Pk yet"
+        assert len(self._var_pairs) < 2, "Bias doesn't support other Pk yet"
         return needs
 
     def _get_Pk_mm(self):
@@ -60,17 +61,65 @@ class Bias(Theory):
         for pair in self._var_pairs:
 
             if self.nonlinear:
-                k, z, Pk_mm = self.provider.get_Pk_grid(var_pair=pair,
+                self.k, self.z, Pk_mm = self.provider.get_Pk_grid(var_pair=pair,
                                                             nonlinear=True)
                 # Pk_mm = np.flip(Pk_nonlin, axis=0)
             else:
-                k, z, Pk_mm = self.provider.get_Pk_grid(var_pair=pair,
+                self.k, self.z, Pk_mm = self.provider.get_Pk_grid(var_pair=pair,
                                                          nonlinear=False)
                 # Pk_mm = np.flip(Pk_lin, axis=0)
 
         return Pk_mm
 
     # def calculate(self, state, want_derived=True, **params_values_dict):
+
+    def get_Pk_gg_interpolator(self, extrap_kmax=None):
+
+        key = ("Pk_gg_interpolator",
+               self.nonlinear, extrap_kmax) + tuple(sorted(self._var_pairs))
+
+        pk = self.get_Pk_gg_grid()
+        log_p = True
+        sign = 1
+        if np.any(pk < 0):
+            if np.all(pk < 0):
+                sign = -1
+            else:
+                log_p = False
+        if log_p:
+            pk = np.log(sign * pk)
+        # elif extrap_kmax > k[-1]:
+        #     raise LoggedError(self.log,
+        #                       'Cannot do log extrapolation with zero-crossing pk '
+        #                       'for %s, %s' % var_pair)
+        result = PowerSpectrumInterpolator(self.z, self.k, pk, logP=log_p, logsign=sign,
+                                           extrap_kmax=extrap_kmax)
+        self.current_state[key] = result
+        return result
+
+    def get_Pk_gm_interpolator(self, extrap_kmax=None):
+
+        key = ("Pk_gm_interpolator",
+               self.nonlinear, extrap_kmax) + tuple(sorted(self._var_pairs))
+
+        pk = self.get_Pk_gm_grid()
+        log_p = True
+        sign = 1
+        if np.any(pk < 0):
+            if np.all(pk < 0):
+                sign = -1
+            else:
+                log_p = False
+        if log_p:
+            pk = np.log(sign * pk)
+        # elif extrap_kmax > k[-1]:
+        #     raise LoggedError(self.log,
+        #                       'Cannot do log extrapolation with zero-crossing pk '
+        #                       'for %s, %s' % var_pair)
+        result = PowerSpectrumInterpolator(self.z, self.k, pk, logP=log_p, logsign=sign,
+                                           extrap_kmax=extrap_kmax)
+        self.current_state[key] = result
+        return result
 
     def get_Pk_gg_grid(self):
         return self._current_state['Pk_gg_grid']
--- soliket/tests/test_bias.py
+++ soliket/tests/test_bias.py
@@ -20,7 +20,7 @@ def test_linear_bias_model():
     from soliket.bias import Linear_bias
 
     info = {"params": {
-                       "b_lin": 1.,
+                       "b_lin": 1.1,
                        "H0": 70.,
                        "ombh2": 0.0245,
                        "omch2": 0.1225,
@@ -44,7 +44,7 @@ def test_linear_bias_compute_grid():
     from soliket.bias import Linear_bias
 
     info = {"params": {
-                       "b_lin": 1.,
+                       "b_lin": 1.1,
                        "H0": 70.,
                        "ombh2": 0.0245,
                        "omch2": 0.1225,
@@ -78,6 +78,53 @@ def test_linear_bias_compute_grid():
 
     Pk_gg = lhood.provider.get_Pk_gg_grid()
     Pk_gm = lhood.provider.get_Pk_gm_grid()
+
+    assert np.allclose(Pk_mm_lin * info["params"]["b_lin"]**2., Pk_gg)
+    assert np.allclose(Pk_mm_lin * info["params"]["b_lin"], Pk_gm)
+
+
+def test_linear_bias_compute_interpolator():
+
+    from soliket.bias import Linear_bias
+
+    info = {"params": {
+                       "b_lin": 1.1,
+                       "H0": 70.,
+                       "ombh2": 0.0245,
+                       "omch2": 0.1225,
+                       "ns": 0.96,
+                       "As": 2.2e-9,
+                       "tau": 0.05
+                       },
+            "likelihood": {"one": None},
+            "theory": {"camb": None,
+                       "linear_bias": {"external": Linear_bias}
+                       },
+            "sampler": {"evaluate": None},
+            "debug": True
+           }
+
+    model = get_model(info)  # noqa F841
+    model.add_requirements({"Pk_grid": {"z": 0., "k_max": 10.,
+                                        "nonlinear": False,
+                                        "vars_pairs": ('delta_tot', 'delta_tot')
+                                        },
+                             "Pk_gg_interpolator": None,
+                             "Pk_gm_interpolator": None
+                            })
+
+    model.logposterior(info['params'])  # force computation of model
+
+    lhood = model.likelihood['one']
+
+    k, z, Pk_mm_lin = lhood.provider.get_Pk_grid(var_pair=('delta_tot', 'delta_tot'),
+                                                 nonlinear=False)
+
+    Pk_gg = lhood.provider.get_Pk_gg_interpolator().P
+    Pk_gg = Pk_gg(z, k)
+
+    Pk_gm = lhood.provider.get_Pk_gm_interpolator().P
+    Pk_gm = Pk_gm(z, k)
 
     assert np.allclose(Pk_mm_lin * info["params"]["b_lin"]**2., Pk_gg)
     assert np.allclose(Pk_mm_lin * info["params"]["b_lin"], Pk_gm)
