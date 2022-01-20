aca42adcb4c81fc1f87de2d6cc07f12e426c9993
Ian Harrison <itrharrison@gmail.com>
2022-01-20 16:46:12 +0000

    check with hsc instead

--- soliket/tests/test_cross_correlation.py
+++ soliket/tests/test_cross_correlation.py
@@ -36,22 +36,22 @@ def test_galaxykappa():
 
 def test_shearkappa():
     cosmo_params = {
-        "Omega_c": 0.31,
-        "Omega_b": 0.04,
+        "Omega_c": 0.267,
+        "Omega_b": 0.0497,
         "h": 0.67,
-        "n_s": 0.97
+        "n_s": 0.964
     }
 
     info = {"params": {"omch2": cosmo_params['Omega_c'] * cosmo_params['h'] ** 2.,
                        "ombh2": cosmo_params['Omega_b'] * cosmo_params['h'] ** 2.,
                        "H0": cosmo_params['h'] * 100,
                        "ns": cosmo_params['n_s'],
-                       "As": 2.2e-9 * 0.81 / 0.8,
-                       "tau": 0,
-                       "A_IA": 0.54},
+                       "As": 2.1e-9,
+                       "tau": 0.054,
+                       "A_IA": 0.0},
             "likelihood": {"ShearKappaLikelihood":
                             {"external": ShearKappaLikelihood,
-                             "dndz_file": "soliket/data/xcorr_simulated/dndz_kids1000-highz.txt"}  # noqa E501
+                             "dndz_file": "soliket/data/xcorr_simulated/dndz_hsc.txt"}  # noqa E501
                           },
             "theory": {
                 "camb": None,
@@ -66,7 +66,7 @@ def test_shearkappa():
 
     # pdb.set_trace()
 
-    ells = np.linspace(50, 3000, 128)
+    ells = np.linspace(50, 5000, 128)
 
     # ell_load = lhood.data.x
     ell_load = np.concatenate([ells, ells])
@@ -87,25 +87,28 @@ def test_shearkappa():
     cl_kappagamma = cl_theory[:n_ell]
 
     from matplotlib import pyplot as plt
-    plt.figure(1, figsize=(2 * 4.5, 3.75))
-    plt.subplot(121)
-    plt.plot(ell_obs_gammagamma, ell_obs_gammagamma * cl_gammagamma * 1.e6)
-    plt.title(r'$\gamma \gamma$')
+    # plt.figure(1, figsize=(2 * 4.5, 3.75))
+    plt.figure(1, figsize=(4.5, 3.75))
+    # plt.subplot(121)
+    # plt.plot(ell_obs_gammagamma, ell_obs_gammagamma * cl_gammagamma * 1.e6)
+    # plt.title(r'$\gamma \gamma$')
+    # plt.ylabel(r'$\ell C_{\ell} / 10^6$')
+    # plt.xlabel(r'$\ell$')
+    # # plt.xscale('log')
+    # # plt.ylim([-0.5, 1.6])
+    # plt.xlim([60, 2000])
+    # plt.axhline(0, linestyle='dashed', alpha=0.4)
+    # plt.subplot(122)
+    plt.plot(ell_obs_kappagamma, ell_obs_kappagamma * cl_kappagamma * 1.e6)
+    # np.savetxt('./plots/kids1000-lowz.txt', np.column_stack([ell_obs_kappagamma, cl_kappagamma]))
+    # plt.title(r'$\kappa \gamma$, $C_{\ell = 0}'+r' = {:.2f}$'.format(ell_obs_kappagamma[0] * cl_kappagamma[0] * 1.e6)))
+    plt.title(r'$\kappa \gamma$ HSC')
     plt.ylabel(r'$\ell C_{\ell} / 10^6$')
     plt.xlabel(r'$\ell$')
-    plt.xscale('log')
-    # plt.ylim([-0.5, 1.6])
-    plt.xlim([60, 3000])
-    plt.axhline(0, linestyle='dashed', alpha=0.4)
-    plt.subplot(122)
-    plt.plot(ell_obs_kappagamma, ell_obs_kappagamma * cl_kappagamma * 1.e6)
-    plt.title(r'$\kappa \gamma$')
-    # plt.ylabel(r'$\ell C_{\ell} / 10^6$'))
-    plt.xlabel(r'$\ell$')
-    plt.xscale('log')
+    # plt.xscale('log')
     plt.ylim([-0.5, 1.6])
-    plt.xlim([60, 3000])
+    plt.xlim([60, 2000])
     plt.axhline(0, color='k', linestyle='dashed', alpha=0.4)
-    # plt.savefig('./plots/kappagamma.png', dpi=300, bbox_inches='tight')
+    plt.savefig('./plots/kappagamma-hsc.png', dpi=300, bbox_inches='tight')
 
     # assert np.isclose(loglikes[0], 88.2, atol=.2, rtol=0.)
