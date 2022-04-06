05160014ee817e75fb5341739d30e842f969e06a
Emily Moser <emilymoser@Emilys-MacBook-Air-2.local>
2022-04-06 15:16:48 -0400

    update test script

--- soliket/szlike/test_szlike.py
+++ soliket/szlike/test_szlike.py
@@ -1,46 +1,66 @@
 import numpy as np
-from soliket.szlike import SZLikelihood
+from soliket.szlike import KSZLikelihood, TSZLikelihood
 from cobaya.model import get_model
 
 
-previously_calculated_differences_ksz = [ksz_data - gnfw_I_calculate_outside_SOliket]
-previously_calculated_differences_tsz = [tsz_data - gnfw_I_calculate_outside_SOliket] 
+sz_data_file = '/Users/emilymoser/Desktop/SOLikeT/soliket/szlike/TNG_data.txt'
+gnfw_test_file = '/Users/emilymoser/Desktop/SOLikeT/soliket/szlike/gnfw_test_projections.txt'
+cov_ksz_file= '/Users/emilymoser/Desktop/SOLikeT/soliket/szlike/cov_diskring_ksz_varweight_bootstrap.txt'
+cov_tsz_file= '/Users/emilymoser/Desktop/SOLikeT/soliket/szlike/cov_diskring_tsz_varweight_bootstrap.txt'
+
+ksz_data,tsz_data = np.loadtxt(sz_data_file,usecols=(1,2),unpack=True)
+ksz_gnfw,tsz_gnfw = np.loadtxt(gnfw_test_file,usecols=(1,2),unpack=True)
+
+ksz_differences = ksz_data - ksz_gnfw
+tsz_differences = tsz_data - tsz_gnfw 
 
 def test_ksz():
-    #I'm not sure what is necessary here, check others for examples. Do we need yaml?
-    info = {"likelihood": {"SZLikelihood":
-                            {"external": SZlikelihood,
-                             "beam_file": "soliket/tests/data/beam_f150_daynight.txt" ###come back to this
+    info = {"params": {"Omega_m": 0.25,
+                       "Omega_b": 0.044,
+                       "hh": 0.7,
+                       "Omega_L": 0.75,
+                       "rhoc_0": 2.77525e2,
+                       "C_OVER_HUBBLE": 2997.9,
+                       "XH": 0.76,
+                       "v_rms": 1.06e-3,
+                       "gnfw_rho0":1536.17633526,
+                       "gnfw_al_ksz":1.02481552231,
+                       "gnfw_bt_ksz":3.45493977635,
+                       "gnfw_A2h_ksz":1.0,
+                       "gnfw_P0":9.10766709141,
+                       "gnfw_xc_tsz":0.698455690636,
+                       "gnfw_bt_tsz":4.76625102519,
+                       "gnfw_A2h_tsz":1.0},
+            "likelihood": {"KSZLikelihood":
+                            {"external": KSZLikelihood,
                              }
-                          },
-            "theory": {
-                "camb": None, #idk what these are for??
-            },
-            "debug": False, "stop_at_error": True}
+                          }}
 
     model = get_model(info)
     loglikes, derived = model.loglikes()
 
-    lhood = model.likelihood['SZLikelihood']
+    lhood = model.likelihood['KSZLikelihood']
 
-    thta_arc, ksz_data = np.loadtxt('soliket/tests/data/TNG_data.txt', usecols=(0,1),unpack=True) #should this be in tests for folder for szlike?
-    cov_ksz = np.loadtxt('soliket/tests/data/....txt')
+    thta_arc, ksz_data = np.loadtxt(sz_data_file, usecols=(0,1),unpack=True)
+    cov_ksz = np.loadtxt(cov_ksz_file)
 
     lhood.thta_arc = thta_arc
     lhood.ksz_data = ksz_data
-    lhood.cov_ksz = cov_ksz #check what we call this in szlike? might be dy? check units
+    lhood.dy_ksz = np.sqrt(np.diag(cov_ksz)) * 3282.8 * 60.**2
 
     ksz_theory = lhood._get_theory(**info["params"])
 
     diff_theory = ksz_data - ksz_theory 
 
-    assert np.array_equal(previously_calculated_differences_ksz, diff_theory)
+    print("my gnfw",ksz_gnfw)
+    print("SO calc gnfw",ksz_theory)
+    assert np.array_equal(ksz_differences, diff_theory)
 
-
+'''
 def test_tsz():
     #I'm not sure what is necessary here, check others for examples. Do we need yaml?
     info = {"likelihood": {"SZLikelihood":
-                            {"external": SZlikelihood,
+                            {"external": TSZLikelihood,
                              "beam_file": "soliket/tests/data/beam_f150_daynight.txt" ###come back to this
                              }
                           },
@@ -52,7 +72,7 @@ def test_tsz():
     model = get_model(info)
     loglikes, derived = model.loglikes()
 
-    lhood = model.likelihood['SZLikelihood']
+    lhood = model.likelihood['TSZLikelihood']
 
     thta_arc, tsz_data = np.loadtxt('soliket/tests/data/TNG_data.txt', usecols=(0,2),unpack=True) #should this be in tests for folder for szlike?
     cov_tsz = np.loadtxt('soliket/tests/data/....txt')
@@ -65,6 +85,6 @@ def test_tsz():
 
     diff_theory = tsz_data - tsz_theory 
 
-    assert np.array_equal(previously_calculated_differences_tsz, diff_theory)
-
+    assert np.array_equal(tsz_differences, diff_theory)
+'''
 
