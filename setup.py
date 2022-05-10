68c3c6f349984505f698c165189c8cf961dfaced
Ian Harrison <itrharrison@gmail.com>
2022-05-10 14:36:07 +0100

    added velocileptors branch

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "pyfftw",
         "fgspectra @ git+https://github.com/simonsobs/fgspectra@act_sz_x_cib#egg=fgspectra", # noqa E501
         "mflike @ git+https://github.com/simonsobs/lat_mflike@master",
-        "velocileptors @ https://github.com/sfschen/velocileptors"
+        "velocileptors @ https://github.com/sfschen/velocileptors@master"
     ],
     test_suite="soliket.tests",
     include_package_data=True,
