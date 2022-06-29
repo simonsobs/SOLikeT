33deb66cb91ce9ed0c416d062439c565f90ddb2a
Ian Harrison <itrharrison@gmail.com>
2022-06-29 13:58:46 +0100

    codestyle

--- soliket/clusters/survey.py
+++ soliket/clusters/survey.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from scipy import interpolate
 import astropy.io.fits as pyfits
+
 # from astLib import astWCS
 from astropy.wcs import WCS
 from astropy.io import fits
@@ -18,7 +19,7 @@ def read_clust_cat(fitsfile, qmin):
     Y0 = data.field("y0tilde")
     Y0err = data.field("y0tilde_err")
     ind = np.where(SNR >= qmin)[0]
-    print ("num clust ",np.shape(ind),qmin)
+    print("num clust ", np.shape(ind), qmin)
     return z[ind], zerr[ind], Y0[ind], Y0err[ind]
 
 
@@ -48,6 +49,7 @@ def read_matt_mock_cat(fitsfile, qmin):
     ind = np.where(SNR >= qmin)[0]
     return z[ind], zerr[ind], Y0[ind], Y0err[ind]
 
+
 def read_matt_cat(fitsfile, qmin):
     list = fits.open(fitsfile)
     data = list[1].data
@@ -59,13 +61,15 @@ def read_matt_cat(fitsfile, qmin):
     ind = np.where(SNR >= qmin)[0]
     return z[ind], zerr[ind], Y0[ind], Y0err[ind]
 
+
 def loadAreaMask(extName, DIR):
-    """Loads the survey area mask (i.e., after edge-trimming and point source masking, produced by nemo).
+    """Loads the survey area mask (i.e., after edge-trimming and point source masking,
+     produced by nemo).
     Returns map array, wcs
     """
     areaImg = pyfits.open(os.path.join(DIR, "areaMask%s.fits.gz" % (extName)))
     areaMap = areaImg[0].data
-    wcs = WCS(areaImg[0].header)#, mode="pyfits")
+    wcs = WCS(areaImg[0].header)  # , mode="pyfits")
     areaImg.close()
 
     return areaMap, wcs
@@ -75,9 +79,11 @@ def loadRMSmap(extName, DIR):
     """Loads the survey RMS map (produced by nemo).
     Returns map array, wcs
     """
-    areaImg = pyfits.open(os.path.join(DIR, "RMSMap_Arnaud_M2e14_z0p4%s.fits.gz" % (extName)))
+    areaImg = pyfits.open(
+        os.path.join(DIR, "RMSMap_Arnaud_M2e14_z0p4%s.fits.gz" % (extName))
+    )
     areaMap = areaImg[0].data
-    wcs = WCS(areaImg[0].header)#, mode="pyfits")
+    wcs = WCS(areaImg[0].header)  # , mode="pyfits")
     areaImg.close()
 
     return areaMap, wcs
@@ -86,13 +92,14 @@ def loadRMSmap(extName, DIR):
 def loadQ(source, tileNames=None):
     """Load the filter mismatch function Q as a dictionary of spline fits.
     Args:
-        source (NemoConfig or str): Either the path to a .fits table (containing Q fits for all tiles - this
-            is normally selFn/QFit.fits), or a NemoConfig object (from which the path and tiles to use will
-            be inferred).
-        tileNames (optional, list): A list of tiles for which the Q function will be extracted. If
-            source is a NemoConfig object, this should be set to None.
+        source (NemoConfig or str): Either the path to a .fits table (containing Q fits
+            for all tiles - this is normally selFn/QFit.fits), or a NemoConfig object
+            (from which the path and tiles to use will be inferred).
+        tileNames (optional, list): A list of tiles for which the Q function will be
+        extracted. If source is a NemoConfig object, this should be set to None.
     Returns:
-        A dictionary (with tile names as keys), containing spline knots for the Q function for each tile.
+        A dictionary (with tile names as keys), containing spline knots for the Q
+        function for each tile.
     """
     if type(source) == str:
         combinedQTabFileName = source
@@ -105,20 +112,34 @@ def loadQ(source, tileNames=None):
         combinedQTab = atpy.Table().read(combinedQTabFileName)
         for key in combinedQTab.keys():
             if key != "theta500Arcmin":
-                tckDict[key] = interpolate.splrep(combinedQTab["theta500Arcmin"], combinedQTab[key])
+                tckDict[key] = interpolate.splrep(
+                    combinedQTab["theta500Arcmin"], combinedQTab[key]
+                )
     else:
         if tileNames is None:
             raise Exception(
-                "If source does not point to a complete QFit.fits file, you need to supply tileNames."
+                "If source does not point to a complete QFit.fits file,\
+                you need to supply tileNames."
             )
         for tileName in tileNames:
-            tab = atpy.Table().read(combinedQTabFileName.replace(".fits", "#%s.fits" % (tileName)))
+            tab = atpy.Table().read(
+                combinedQTabFileName.replace(".fits", "#%s.fits" % (tileName))
+            )
             tckDict[tileName] = interpolate.splrep(tab["theta500Arcmin"], tab["Q"])
     return tckDict
 
 
 class SurveyData:
-    def __init__(self, nemoOutputDir, ClusterCat, qmin=5.6, szarMock=False, MattMock=False, tiles=False, num_noise_bins=20):
+    def __init__(
+        self,
+        nemoOutputDir,
+        ClusterCat,
+        qmin=5.6,
+        szarMock=False,
+        MattMock=False,
+        tiles=False,
+        num_noise_bins=20,
+    ):
         self.nemodir = nemoOutputDir
 
         self.tckQFit = loadQ(self.nemodir + "/QFit.fits")
@@ -128,18 +149,28 @@ class SurveyData:
 
         if szarMock:
             print("mock catalog")
-            self.clst_z, self.clst_zerr, self.clst_y0, self.clst_y0err = read_mock_cat(ClusterCat, self.qmin)
+            self.clst_z, self.clst_zerr, self.clst_y0, self.clst_y0err = read_mock_cat(
+                ClusterCat, self.qmin
+            )
         elif MattMock:
             print("Matt mock catalog")
-            self.clst_z, self.clst_zerr, self.clst_y0, self.clst_y0err = read_matt_cat(ClusterCat, self.qmin)
+            self.clst_z, self.clst_zerr, self.clst_y0, self.clst_y0err = read_matt_cat(
+                ClusterCat, self.qmin
+            )
         else:
             print("real catalog")
-            self.clst_z, self.clst_zerr, self.clst_y0, self.clst_y0err = read_clust_cat(ClusterCat, self.qmin)
+            self.clst_z, self.clst_zerr, self.clst_y0, self.clst_y0err = read_clust_cat(
+                ClusterCat, self.qmin
+            )
 
         if tiles:
             self.filetile = self.nemodir + "/tileAreas.txt"
-            self.tilenames = np.loadtxt(self.filetile, dtype=np.str, usecols=0, unpack=True)
-            self.tilearea = np.loadtxt(self.filetile, dtype=np.float, usecols=1, unpack=True)
+            self.tilenames = np.loadtxt(
+                self.filetile, dtype=np.str, usecols=0, unpack=True
+            )
+            self.tilearea = np.loadtxt(
+                self.filetile, dtype=np.float, usecols=1, unpack=True
+            )
 
             self.fsky = []
             self.mask = []
@@ -165,7 +196,9 @@ class SurveyData:
             self.rmstotal = self.rms[self.rms > 0]
             self.fskytotal = 987.5 / 41252.9612
 
-        count_temp, bin_edge = np.histogram(np.log10(self.rmstotal), bins=self.num_noise_bins)
+        count_temp, bin_edge = np.histogram(
+            np.log10(self.rmstotal), bins=self.num_noise_bins
+        )
 
         self.frac_of_survey = count_temp * 1.0 / np.sum(count_temp)
         self.Ythresh = 10 ** ((bin_edge[:-1] + bin_edge[1:]) / 2.0)
