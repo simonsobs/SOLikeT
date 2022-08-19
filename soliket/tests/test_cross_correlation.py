07b87092c21a7e047fe92312f3c47abba97f3df5
Ian Harrison <itrharrison@gmail.com>
2022-08-19 13:53:05 +0100

    sacc files and tracers for CrossCorrelation Likelihood (#58)
    * initial commit with tracers
    
    * bandpower binning
    
    * move sacc data, bpw to parent calss
    
    * use_tracers in input, added bpw
    
    * use des x act toy sim
    
    * expanded tests
    
    * tests account for missing data file
    
    * bug fix on tracer selection
    
    * bug fix on tracer selection (actually this time)
    
    * Shear x kappa nuisance delta z (#67)
    
    * added deltaz nuisance model
    
    * working deltaz nuisance model
    
    * deltaz bug fizes for plotting
    
    * codestyle
    
    * nuisance parm for tracer2
    
    * added m nuisance parameterisation
    
    * codestyle
    
    * fixes to z and m nuisance
    
    * added IA model
    
    * added hmcode test
    
    * codestyle
    
    * added data file for tests
    
    * added shearkappa test against lit result
    
    * removed commented plotting

--- soliket/tests/test_cross_correlation.py
+++ soliket/tests/test_cross_correlation.py
@@ -1,100 +1,154 @@
 import numpy as np
+import pytest
 from soliket.ccl import CCL
-from soliket.cross_correlation import GalaxyKappaLikelihood, ShearKappaLikelihood
 from cobaya.model import get_model
 
+cosmo_params = {"Omega_c": 0.25, "Omega_b": 0.05, "h": 0.67, "n_s": 0.96}
 
-def test_galaxykappa():
-    cosmo_params = {
-        "Omega_c": 0.25,
-        "Omega_b": 0.05,
-        "h": 0.67,
-        "n_s": 0.96
+info = {
+    "params": {
+        "omch2": cosmo_params["Omega_c"] * cosmo_params["h"] ** 2.0,
+        "ombh2": cosmo_params["Omega_b"] * cosmo_params["h"] ** 2.0,
+        "H0": cosmo_params["h"] * 100,
+        "ns": cosmo_params["n_s"],
+        "As": 2.2e-9,
+        "tau": 0,
+        "b1": 1,
+        "s1": 0.4,
+    },
+    "theory": {"camb": None, "ccl": {"external": CCL, "nonlinear": False}},
+    "debug": False,
+    "stop_at_error": True,
+}
+
+
+def test_galaxykappa_import():
+
+    from soliket.cross_correlation import GalaxyKappaLikelihood
+
+
+def test_shearkappa_import():
+
+    from soliket.cross_correlation import ShearKappaLikelihood
+
+
+def test_galaxykappa_model():
+
+    from soliket.cross_correlation import GalaxyKappaLikelihood
+
+    info["likelihood"] = {
+        "GalaxyKappaLikelihood": {"external": GalaxyKappaLikelihood,
+                                  "datapath": None}
     }
 
-    info = {"params": {"omch2": cosmo_params['Omega_c'] * cosmo_params['h'] ** 2.,
-                       "ombh2": cosmo_params['Omega_b'] * cosmo_params['h'] ** 2.,
-                       "H0": cosmo_params['h'] * 100,
-                       "ns": cosmo_params['n_s'],
-                       "As": 2.2e-9,
-                       "tau": 0,
-                       "b1": 1,
-                       "s1": 0.4},
-            "likelihood": {"GalaxyKappaLikelihood": GalaxyKappaLikelihood},
-            "theory": {
-                "camb": None,
-                "ccl": {"external": CCL, "nonlinear": False}
-            },
-            "debug": False, "stop_at_error": True}
+    model = get_model(info) # noqa F841
+
+
+# @pytest.mark.xfail(reason="data file not in repo")
+def test_shearkappa_model():
+
+    from soliket.cross_correlation import ShearKappaLikelihood
+
+    info["likelihood"] = {"ShearKappaLikelihood": {"external": ShearKappaLikelihood}}
+
+    model = get_model(info) # noqa F841
+
+
+def test_galaxykappa_like():
+
+    from soliket.cross_correlation import GalaxyKappaLikelihood
+
+    info["likelihood"] = {
+        "GalaxyKappaLikelihood": {"external": GalaxyKappaLikelihood,
+                                  "datapath": None}
+    }
 
     model = get_model(info)
     loglikes, derived = model.loglikes()
-    assert np.isclose(loglikes[0], 88.2, atol=.2, rtol=0.)
+    assert np.isclose(loglikes[0], 88.2, atol=0.2, rtol=0.0)
 
 
-def test_shearkappa():
-    cosmo_params = {
-        "h": 0.673,
-        "n_s": 0.964
+# @pytest.mark.xfail(reason="data file not in repo")
+def test_shearkappa_like():
+
+    from soliket.cross_correlation import ShearKappaLikelihood
+
+    test_datapath = "soliket/tests/data/cs82_gs-planck_kappa_binned.sim.sacc.fits"
+
+    info["likelihood"] = {
+        "ShearKappaLikelihood": {"external": ShearKappaLikelihood,
+                                 "datapath": test_datapath}
     }
 
-    info = {"params": {#"omch2": cosmo_params['Omega_c'] * cosmo_params['h'] ** 2.,
-                       "omch2": 0.1200,
-                       "ombh2": 0.0223,
-                       "H0": cosmo_params['h'] * 100,
-                       "ns": cosmo_params['n_s'],
-                       "As": 2.1e-9,
-                       "tau": 0.054,
-                       "A_IA": 0.0,
-                       "b1": 1},
-            "likelihood": {"ShearKappaLikelihood":
-                            {"external": ShearKappaLikelihood,
-                             "dndz_file": "soliket/tests/data/dndz_hsc.txt"  # noqa E501
-                             }
-                          },
-            "theory": {
-                "camb": None,
-                "ccl": {"external": CCL, "nonlinear": False}
-            },
-            "debug": False, "stop_at_error": True}
+    # Cosmological parameters for the test data, digitized from
+    # Fig. 3 and Eq. 8 of Hall & Taylor (2014).
+    # See https://github.com/simonsobs/SOLikeT/pull/58 for validation plots
+    info['params'] = {"omch2": 0.118,  # Planck + lensing + WP + highL
+                      "ombh2": 0.0222,
+                      "H0": 68.0,
+                      "ns": 0.962,
+                      "As": 2.1e-9,
+                      "tau": 0.094,
+                      "mnu": 0.0,
+                      "nnu": 3.046,
+                      "s1": 0.4,
+                      "b1": 1.0}
 
     model = get_model(info)
     loglikes, derived = model.loglikes()
 
-    lhood = model.likelihood['ShearKappaLikelihood']
+    assert np.isclose(loglikes, 637.64473666)
 
-    ell_paper, ellcl_paper = np.loadtxt('soliket/tests/data/lcl_hsc.txt', unpack=True)
-    ells = ell_paper
 
-    ell_load = np.concatenate([ells, ells])
-    n_ell = len(ell_load) // 2
+def test_shearkappa_deltaz():
 
-    lhood.ell_auto = ell_load[n_ell:]
-    lhood.ell_cross = ell_load[:n_ell]
+    from soliket.cross_correlation import ShearKappaLikelihood
 
-    ell_obs_kappagamma = ell_load[n_ell:]
-    # ell_obs_gammagamma = ell_load[:n_ell]
+    info["likelihood"] = {"ShearKappaLikelihood": {"external": ShearKappaLikelihood,
+                                                   "z_nuisance_mode": "deltaz"}}
 
-    cl_theory = lhood._get_theory(**info["params"])
-    cl_kappagamma = cl_theory[n_ell:]
-    # cl_gammagamma = cl_theory[:n_ell]
+    model = get_model(info) # noqa F841
+    loglikes, derived = model.loglikes()
 
-    # from matplotlib import pyplot as plt
-    # plt.figure(1, figsize=(4.5, 3.75))
-    # plt.plot(ell_obs_kappagamma, ell_obs_kappagamma * cl_kappagamma * 1.e6,
-    #          label='Calculated')
-    # plt.plot(ell_paper, ellcl_paper, '--',
-    #          label='Marques et al, Planck 2018 $A=1$')
-    # plt.title(r'$\kappa \gamma$ HSC')
-    # plt.ylabel(r'$\ell C_{\ell} / 10^6$')
-    # plt.xlabel(r'$\ell$')
-    # # plt.xscale('log')
-    # plt.legend(loc='upper right')
-    # plt.ylim([-0.5, 1.6])
-    # plt.xlim([60, 2000])
-    # plt.axhline(0, color='k', linestyle='dashed', alpha=0.4)
-    # plt.savefig('./plots/kappagamma-hsc.png', dpi=300, bbox_inches='tight')
+    assert np.isfinite(loglikes)
 
-    assert np.allclose(ell_obs_kappagamma * cl_kappagamma * 1.e6,
-                       ellcl_paper,
-                       atol=.2, rtol=0.)
+
+def test_shearkappa_m():
+
+    from soliket.cross_correlation import ShearKappaLikelihood
+
+    info["likelihood"] = {"ShearKappaLikelihood": {"external": ShearKappaLikelihood,
+                                                   "m_nuisance_mode": True}}
+
+    model = get_model(info) # noqa F841
+    loglikes, derived = model.loglikes()
+
+    assert np.isfinite(loglikes)
+
+
+def test_shearkappa_ia():
+
+    from soliket.cross_correlation import ShearKappaLikelihood
+
+    info["likelihood"] = {"ShearKappaLikelihood": {"external": ShearKappaLikelihood,
+                                                   "ia_mode": True}}
+
+    model = get_model(info) # noqa F841
+    loglikes, derived = model.loglikes()
+
+    assert np.isfinite(loglikes)
+
+
+def test_shearkappa_hmcode():
+
+    from soliket.cross_correlation import ShearKappaLikelihood
+
+    info["likelihood"] = {"ShearKappaLikelihood": ShearKappaLikelihood}
+    info["theory"] = {"camb": {'extra_args': {'halofit_version': 'mead2020_feedback',
+                                              'HMCode_logT_AGN': 7.8}},
+                      "ccl": {"external": CCL, "nonlinear": False}}
+
+    model = get_model(info) # noqa F841
+    loglikes, derived = model.loglikes()
+
+    assert np.isfinite(loglikes)
