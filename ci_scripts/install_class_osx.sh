0281569ca881288d4e144ff0660d109598713431
Ian Harrison <itrharrison@gmail.com>
2022-12-06 12:23:31 +0000

    changes to test env

--- ci_scripts/install_class_osx.sh
+++ ci_scripts/install_class_osx.sh
@@ -5,16 +5,20 @@ export CONDA_BUILD_SYSROOT=/
 rm -rf class_public
 git clone --depth=1000 https://github.com/lesgourg/class_public.git
 cd class_public
+git checkout v2.7.2
 
-# if you are running on macos 11.6 and higher, uncomment the following line
-# sed -i.bak -e 's/gcc/gcc-9/g' Makefile
-sed -i.bak -e 's/^OMPFLAG/#OMPFLAG/g' Makefile
+echo "----------------------------------"
+env
+echo "----------------------------------"
 
-# if you are running on macos 10.15 and lower, uncomment the following 4 lines
-#sed -i.bak -e 's/^CC/#CC/g' Makefile
-#sed -i.bak -e 's/^OPTFLAG =/OPTFLAG = ${CFLAGS} ${LDFLAGS}/g' Makefile
-#sed -i.bak -e 's/^#CCFLAG +=/CCFLAG +=/g' Makefile
-#sed -i.bak -e 's/^#CCFLAG =/CCFLAG =/g' Makefile
+sed -i.bak -e 's/^CC/#CC/g' Makefile
+sed -i.bak -e 's/^OPTFLAG =/OPTFLAG = ${CFLAGS} ${LDFLAGS}/g' Makefile
+sed -i.bak -e 's/^#CCFLAG +=/CCFLAG +=/g' Makefile
+sed -i.bak -e 's/^#CCFLAG =/CCFLAG =/g' Makefile
+
+echo "----------------------------------"
+cat Makefile
+echo "----------------------------------"
 
 make -j4
 
