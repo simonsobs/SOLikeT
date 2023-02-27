bf4e8867fe0a20accd246ee32ce308cd91316b16
Frank Qu <frankqu@Franks-MacBook-Pro.local>
2023-02-27 21:23:37 +0100

    add test checking for log-likelihood at a fixed point

--- soliket/tests/test_lensing.py
+++ soliket/tests/test_lensing.py
@@ -18,6 +18,40 @@ packages_path = os.environ.get("COBAYA_PACKAGES_PATH") or os.path.join(
     tempfile.gettempdir(), "lensing_packages"
 )
 
+fiducial_params = {
+    "ombh2": 0.02219218,
+    "omch2": 0.1203058,
+    "H0": 67.02393,
+    "tau": 0.6574325e-01,
+    "nnu": 3.046,
+    "num_massive_neutrinos": 1,
+    "As": 2.15086031154146e-9,
+    "ns": 0.9625356e00,
+}
+
+info={"theory": {"camb": {"extra_args": {"kmax": 0.9}}}}
+#info['params']=fiducial_params
+
+
+def test_lensing_import(request):
+    from soliket.lensing import lensing
+
+
+def test_lensing_like(request):
+
+    from soliket.lensing import lensing
+
+    info["likelihood"] = {
+        "LensLikelihood": {"external": lensing.LensingLikelihood},
+        "soliket.utils.OneWithCls": {"lmax": 10000},
+
+    }
+    info['params']=fiducial_params
+    model = get_model(info)
+    loglikes, derived = model.loglikes()
+    assert np.isclose(loglikes[0], 335.85600978, atol=0.2, rtol=0.0)  
+
+
 
 def get_demo_lensing_model(theory):
     if theory == "camb":
