36b4accd810caf079125512b09751220763470f5
Hidde Jense <JenseH@cardiff.ac.uk>
2023-02-27 13:55:51 +0100

    Added sphinx extensions.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,10 @@ author = 'The SO Collaboration'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc", # Generate doc pages from source docstrings
+    "sphinx.ext.viewcode", # Generate links to source code
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +26,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
