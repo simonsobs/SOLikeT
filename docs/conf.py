08c2f7e22ef57809fb2f13a263694dbfa1b98b0a
Hidde Jense <JenseH@cardiff.ac.uk>
2023-02-27 16:50:13 +0100

    Added SOLikeT import (for autodoc).

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# Import SOLikeT (for autodoc)
+import sys
+sys.path.insert(0, "..")
+import soliket
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
