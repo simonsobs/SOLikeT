baad41f84c168d88fcd2bf70c7de14d7571724d7
Frank Qu <frankqu@Franks-MacBook-Pro.local>
2023-02-28 13:54:20 +0100

    fix test run import

--- soliket/tests/test_runs.py
+++ soliket/tests/test_runs.py
@@ -10,7 +10,6 @@ packages_path = os.environ.get("COBAYA_PACKAGES_PATH") or os.path.join(
     tempfile.gettempdir(), "lensing_packages"
 )
 
-
 @pytest.mark.parametrize("lhood",
                          ["mflike",
                           "lensing",
@@ -21,8 +20,8 @@ packages_path = os.environ.get("COBAYA_PACKAGES_PATH") or os.path.join(
                           ])
 def test_evaluate(lhood):
 
-    if lhood == "lensing" or lhood == "multi":
-        pytest.xfail(reason="lensing lhood install failure")
+    # if lhood == "lensing" or lhood == "multi":
+    #     pytest.xfail(reason="lensing lhood install failure")
 
     if lhood == "mflike":
         pytest.skip(reason="don't want to install 300Mb of data!")
@@ -33,6 +32,9 @@ def test_evaluate(lhood):
     info = yaml_load(pkgutil.get_data("soliket", f"tests/test_{lhood}.yaml"))
     info["force"] = True
     info['sampler'] = {'evaluate': {}}
+
+    from cobaya.install import install
+    install(info, path=packages_path, skip_global=True)
 
     updated_info, sampler = run(info)
 
@@ -47,8 +49,8 @@ def test_evaluate(lhood):
                           ])
 def test_mcmc(lhood):
 
-    if lhood == "lensing" or lhood == "multi":
-        pytest.xfail(reason="lensing lhood install failure")
+    # if lhood == "lensing" or lhood == "multi":
+    #     pytest.xfail(reason="lensing lhood install failure")
 
     if lhood == "mflike":
         pytest.skip(reason="don't want to install 300Mb of data!")
@@ -59,5 +61,8 @@ def test_mcmc(lhood):
     info = yaml_load(pkgutil.get_data("soliket", f"tests/test_{lhood}.yaml"))
     info["force"] = True
     info['sampler'] = {'mcmc': {'max_samples': 10, 'max_tries': 1000}}
+
+    from cobaya.install import install
+    install(info, path=packages_path, skip_global=True)
 
     updated_info, sampler = run(info)
