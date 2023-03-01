a079e8e10724c07273ed56f54e70f838cb69fd43
Hidde Jense <JenseH@cardiff.ac.uk>
2023-03-01 15:06:45 +0100

    Added distance test.

--- soliket/tests/test_ccl.py
+++ soliket/tests/test_ccl.py
@@ -2,14 +2,19 @@
 Check that CCL works correctly.
 """
 import pytest
+import numpy as np
 from cobaya.model import get_model
 from cobaya.likelihood import Likelihood
 
 
 class TestLike(Likelihood):
+    """
+    This is a mock likelihood that simply forces soliket.CCL to calculate
+    a CCL object.
+    """
     def logp(self, **params_values):
         ccl = self.theory.get_CCL() # noqa F841
-        return 0.0
+        return -1.0
 
     def get_requirements(self):
         return {"CCL": None}
@@ -21,8 +26,7 @@ fiducial_params = {
     "tau": 0.065,
     "ns": 0.9645,
     "logA": 3.07,
-    # derived params
-    "As": {"value": "lambda logA: 1e-10 * np.exp(logA)"},
+    "As": {"value": "lambda logA: 1e-10*np.exp(logA)"}
 }
 
 info_dict = {
@@ -34,7 +38,7 @@ info_dict = {
         "camb": {
         },
         "soliket.CCL": {
-            "kmax": 10.0
+            "kmax": 10.0,
         }
     }
 }
@@ -52,4 +56,25 @@ def test_ccl_cobaya(request):
     Test whether we can call CCL from cobaya.
     """
     model = get_model(info_dict)
+    model.loglikes()
+
+
+# Test if angular diameter distance & lum distance have good propto.
+def test_ccl_distances(request):
+    """
+    Test whether the calculated angular diameter distance & luminosity distances
+    in CCL have the correct relation.
+    """
+    model = get_model(info_dict)
     model.loglikes({})
+    cosmo = model.provider.get_CCL()["cosmo"]
+    
+    z = np.linspace(0.0, 10.0, 100)
+    a = 1.0 / (z + 1.0)
+    
+    da = cosmo.angular_diameter_distance(a)
+    dl = cosmo.luminosity_distance(a)
+    
+    assert np.allclose(da * (1.0 + z) ** 2.0, dl)
+
+# Test that Pk-nonlin > Pk-lin in expected regimes.
