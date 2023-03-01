c7bc0399efae4dbc839bf74f1499548cc4c72a9f
Ian Harrison <itrharrison@gmail.com>
2023-03-01 16:35:54 +0100

    changed inits

--- soliket/__init__.py
+++ soliket/__init__.py
@@ -4,6 +4,7 @@ from .gaussian import GaussianLikelihood, MultiGaussianLikelihood  # noqa: F401
 from .ps import PSLikelihood, BinnedPSLikelihood  # noqa: F401
 from .mflike import MFLike  # noqa: F401
 from .mflike import TheoryForge_MFLike
+from .cross_correlation import CrossCorrelationLikelihood, GalaxyKappaLikelihood, ShearKappaLikelihood  # noqa: F401, E501
 from .xcorr import XcorrLikelihood  # noqa: F401
 from .foreground import Foreground
 from .bandpass import BandPass
@@ -13,12 +14,4 @@ try:
     from .clusters import ClusterLikelihood  # noqa: F401
 except ImportError:
     print('Skipping cluster likelihood (is pyCCL installed?)')
-    pass
-
-try:
-    import pyccl as ccl  # noqa: F401
-    from .ccl import CCL  # noqa: F401
-    from .cross_correlation import GalaxyKappaLikelihood, ShearKappaLikelihood  # noqa: F401, E501
-except ImportError:
-    print('Skipping CCL module as pyCCL is not installed')
     pass
