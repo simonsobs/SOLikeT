47f38b3888a14f543a13b5b41dbeb93cfb9114da
borisbolliet <boris.bolliet@gmail.com>
2023-04-06 18:28:13 +0300

    Update __init__.py

--- soliket/__init__.py
+++ soliket/__init__.py
@@ -3,7 +3,7 @@ from .gaussian import GaussianLikelihood, MultiGaussianLikelihood
 # from .ps import PSLikelihood, BinnedPSLikelihood
 # from .clusters import ClusterLikelihood
 # from .mflike import MFLike
-from .ccl import CCL
+# from .ccl import CCL
 from .ymap.ymap_ps import SZLikelihood, SZForegroundTheory
 #from .ymap.classy_sz import classy_sz
 from .sz_binned_cluster_counts.binned_cc import binned_cc_likelihood
