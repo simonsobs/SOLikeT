9b0dd0b3a8dd3aafbecc871a30b038534464e891
Martina Gerbino <bradamante@martina.local>
2023-06-09 18:30:59 +0200

    cosmetics in test_utils

--- soliket/tests/test_utils.py
+++ soliket/tests/test_utils.py
@@ -2,9 +2,10 @@ import numpy as np
 
 from soliket.utils import binner
 
+
 def test_binning():
 
-	values_test = np.array([ 1. ,  4. ,  7. , 10.5])
+	values_test = np.array([1.,  4.,  7., 10.5])
 	centers_test = np.array([1.5, 4.5, 7.5, 10.5])
 
 	bincent, binval = binner(np.arange(13), np.arange(13), np.arange(13, step=3))
