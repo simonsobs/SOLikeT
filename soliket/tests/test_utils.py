06bd90f76101ad0d5ad9bf52cd40402c5602171d
Martina Gerbino <bradamante@martina.local>
2023-06-12 11:12:05 +0200

    added test for binner in utils

--- soliket/tests/test_utils.py
+++ soliket/tests/test_utils.py
@@ -3,12 +3,28 @@ import numpy as np
 from soliket.utils import binner
 
 
+def naive_binner(bmin, bmax, x, tobin):
+
+	binned = list()
+	bcent = list()
+	for bm, bmx in zip(bmin, bmax):
+		bcent.append(0.5 * (bmx + bm))
+		binned.append(np.mean(tobin[np.where((x >= bm) & (x <= bmx))[0]]))
+
+	return (np.array(bcent), np.array(binned))
+
+
 def test_binning():
 
-	values_test = np.array([1.,  4.,  7., 10.5])
-	centers_test = np.array([1.5, 4.5, 7.5, 10.5])
+	bmin = np.arange(10, step=3)
+	bmax = np.array([2, 5, 8, 12])
+	binedge = np.arange(13, step=3)
+	ell = np.arange(13)
+	cell = np.arange(13)
 
-	bincent, binval = binner(np.arange(13), np.arange(13), np.arange(13, step=3))
+	centers_test, values_test = naive_binner(bmin, bmax, ell, cell)
 
-	assert np.allclose(bincent, centers_test)
+	bincent, binval = binner(ell, cell, binedge)
+
+	#assert np.allclose(bincent, centers_test)
 	assert np.allclose(binval, values_test)
