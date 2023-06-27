c089543d4ec0e9ab9e2f08a68e8db228ca17dcc0
Martina Gerbino <bradamante@martina.local>
2023-06-27 11:38:38 +0200

    cosmetics

--- soliket/utils.py
+++ soliket/utils.py
@@ -16,18 +16,18 @@ from cobaya.likelihoods.one import one
 
 def binner(ls, cls, bin_edges):
     r"""
-        Simple binning function.
-        :param ls: Axis along which to bin
-        :param cls: Values to be binned
-        :param bin_edges: The edges of the bins. Note that all but the last bin
-                          are open to the right. The last bin is closed. 
+    Simple binning function.
+    :param ls: Axis along which to bin
+    :param cls: Values to be binned
+    :param bin_edges: The edges of the bins. Note that all but the last bin
+                      are open to the right. The last bin is closed. 
 
-        :return: The centers of the bins and the average of ``cls`` within the bins.
-                 Note that the centers are computed as :math:`0.5(edge_min+edge_max)`,
-                 where ``edge_min`` and ``edge_max`` are the bin edges. While this is 
-                 ok for plotting purposes, the user may need to recompute the bin center
-                 in case of integer ``ls`` if the correct baricenter is needed.
-        """
+    :return: The centers of the bins and the average of ``cls`` within the bins.
+             Note that the centers are computed as :math:`0.5(edge_min+edge_max)`,
+             where ``edge_min`` and ``edge_max`` are the bin edges. While this is 
+             ok for plotting purposes, the user may need to recompute the bin center
+             in case of integer ``ls`` if the correct baricenter is needed.
+    """
     x = ls.copy()
     y = cls.copy()
     cents = (bin_edges[:-1] + bin_edges[1:]) / 2.0
