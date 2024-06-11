4e2b9ca98c7418ee24c2baca229ba4137ebc1ac7
Ian Harrison <itrharrison@gmail.com>
2024-06-11 13:21:41 +0100

    try removing conftest

--- soliket/tests/conftest.py
+++ soliket/tests/conftest.py
@@ -1,24 +1,24 @@
-import pytest
+# import pytest
 
 
-@pytest.fixture
-def test_cosmology_params():
-    params = {}
-    params["As"] = 2.15086031154146e-9
-    params["ns"] = 0.9625356
-    params["ombh2"] = 0.02219218
-    params["omch2"] = 0.1203058
-    params["H0"] = 67.02393
-    params["tau"] = 0.06574325
-    params["nnu"] = 3.04
-    params["mnu"] = 0.06
-    return params
+# @pytest.fixture
+# def test_cosmology_params():
+#     params = {}
+#     params["As"] = 2.15086031154146e-9
+#     params["ns"] = 0.9625356
+#     params["ombh2"] = 0.02219218
+#     params["omch2"] = 0.1203058
+#     params["H0"] = 67.02393
+#     params["tau"] = 0.06574325
+#     params["nnu"] = 3.04
+#     params["mnu"] = 0.06
+#     return params
 
 
-@pytest.fixture
-def evaluate_one_info():
-    info = {}
-    info["likelihood"] = {"one": None}
-    info["sampler"] = {"evaluate": None}
-    info["debug"] = True
-    return info
+# @pytest.fixture
+# def evaluate_one_info():
+#     info = {}
+#     info["likelihood"] = {"one": None}
+#     info["sampler"] = {"evaluate": None}
+#     info["debug"] = True
+#     return info
