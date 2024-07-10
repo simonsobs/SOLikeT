12c4828625896e1a428739e1209d95c5b7e71679
Raphaël Kou <raphael.kou@apc.in2p3.fr>
2024-07-10 16:53:02 +0100

    Adding priors to run chains

--- notebooks/dev/create_cosmo_grid.py
+++ notebooks/dev/create_cosmo_grid.py
@@ -7,7 +7,12 @@ from cobaya.yaml import yaml_load_file
 import copy
 
 default: InputDict = {
-    'sampler': {'mcmc': {'Rminus1_stop': 0.1, 'learn_every': '20d', 'covmat': 'auto'}},
+    'sampler': {'mcmc': {'Rminus1_stop': 0.01,
+                         'Rminus1_cl_stop': 0.2,
+                         'drag': False,
+                         'max_tries': 10000,
+                         'oversample_power': 0.4,
+                         'proposal_scale': 1.9}},
 }
 
 # dict or list of default settings to combine; each item can be dict or yaml file name
@@ -20,18 +25,21 @@ getdist_options = {'ignore_rows': 0.3}
 MFLike: InputDict = {'likelihood': yaml_load_file('like_mflike.yaml'),
                      'params': yaml_load_file('params_cosmo_smooth.yaml') | \
                                yaml_load_file('params_mflikefg_smooth.yaml') | \
-                               yaml_load_file('params_mflikecal_smooth.yaml'),
+                               yaml_load_file('params_mflikesyst_smooth.yaml'),
                      'theory': yaml_load_file('theory_camb.yaml') | \
                                yaml_load_file('theory_ccl.yaml') | \
                                yaml_load_file('theory_bandpass.yaml') | \
                                yaml_load_file('theory_foregrounds.yaml') | \
-                               yaml_load_file('theory_theoryforge.yaml')
+                               yaml_load_file('theory_theoryforge.yaml'),
+                     'priors': yaml_load_file('prior_mflikecosmo_smooth.yaml') | \
+                               yaml_load_file('priors_mflikesyst_smooth.yaml')
                     }
 
 LensingLike: InputDict = {'likelihood':  yaml_load_file('like_LensingLikelihood.yaml'),
                           'params': yaml_load_file('params_cosmo_smooth.yaml'),
                           'theory': yaml_load_file('theory_camb.yaml') | \
-                                    yaml_load_file('theory_ccl.yaml')
+                                    yaml_load_file('theory_ccl.yaml'),
+                          'priors': yaml_load_file('prior_mflikecosmo_smooth.yaml')
                         }
 
 joint = DataSet(['MFLike', 'LensingLike'], [MFLike, copy.deepcopy(LensingLike)])
