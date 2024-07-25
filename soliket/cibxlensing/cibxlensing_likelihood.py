daeb57761d970107869d94f54935175bd1b4055d
Yogesh <ymehta3@niagara>
2024-07-25 12:18:46 -0400

    added namaster mode decoupling

--- soliket/cibxlensing/cibxlensing_likelihood.py
+++ soliket/cibxlensing/cibxlensing_likelihood.py
@@ -22,7 +22,7 @@ import os
 from scipy.ndimage.interpolation import shift
 from typing import Optional, Sequence
 from pkg_resources import resource_filename
-from myfuncs import alm as yalm
+from myfuncs import alm as yalm, io as yio
 from mpi4py import MPI
 
 
@@ -32,10 +32,9 @@ class CIBxKAPPA_Likelihood(GaussianLikelihood):
     cib_spectra_file: Optional[str] = 'toy_data.npy'
     cib_cov_directory: Optional[str] = '/project/r/rbond/ymehta3/output/param-fitting/'
     # cib_cov_file: Optional[str] = 'covCl_avg_v3.npy'
-    cib_cov_file: Optional[str] = 'toy_cov_fudge.npy'
-    cross_wsp_directory: Optional[str] = '/project/r/rbond/ymehta3/output/mask_decoupled/mcms/'
-    cross_wsp_file: Optional[str] = 'wsp_DR6_x_545_4.0e+20_gp40.fits'
-    cov_ell_info_directory: Optional[str] = '/project/r/rbond/ymehta3/' 
+    cib_cov_file: Optional[str] = 'toy_cov.npy'
+    project_directory: Optional[str] = '/project/r/rbond/ymehta3/' 
+    cross_mcms_names_file: Optional[str] = 'input_data/names_mcms_dr6_cross.txt'
     cov_ell_info_file: Optional[str] = 'input_data/bandpower_ell_info.txt'
 
     def initialize(self):
@@ -47,14 +46,15 @@ class CIBxKAPPA_Likelihood(GaussianLikelihood):
         #Load the Data
         Dpoints = np.load(self.data_directory + self.data_file)
         self.cov = np.load(os.path.join(self.cov_directory, self.cov_file))
-        self.wsp_name = os.path.join(self.cross_wsp_directory, self.cross_wsp_file)
+        self.mcms_paths_list = yio.readTxt( os.path.join(self.project_directory, self.cross_mcms_names_file) )
 
         #Get Ell Info
-        self.lmax, self.binsize = np.loadtxt(os.path.join(self.cov_ell_info_directory, self.cov_ell_info_file))
+        self.lmax, self.binsize = np.loadtxt(os.path.join(self.project_directory, self.cov_ell_info_file))
 
         #Extract the Data
         self.datavector = Dpoints[:,1]
         self.ellsvector = Dpoints[:,0]  # for mock data, this is binned class_sz theory ells
+        # np.save('/scratch/r/rbond/ymehta3/'+'actual_mock_data.npy', np.stack([self.ellsvector, self.datavector, np.sqrt(np.diag(self.cov))], axis=-1) )
 
         self.debug_index = 1
         comm = MPI.COMM_WORLD
@@ -81,6 +81,7 @@ class CIBxKAPPA_Likelihood(GaussianLikelihood):
 
         #Extract All Cross Spectra
         freq_list = np.sort(np.array(list(theory.keys())).astype(int))
+        freq_list = [str(nu) for nu in freq_list]
         for nu in freq_list:
             Dl_1h_theory = np.array(theory[str(nu)]['1h'])
             Dl_2h_theory = np.array(theory[str(nu)]['2h'])
@@ -93,33 +94,36 @@ class CIBxKAPPA_Likelihood(GaussianLikelihood):
 
         #Bin Theory
         theoryvector_binned = []
-        for Cls_unbinned in theoryvector_unbinned:
+        for iCl, Cls_unbinned in enumerate(theoryvector_unbinned):
             #Interpolate the Theory Vector 
             full_ells = np.arange(self.lmax+1)
             interp_Cls_unbinned = np.interp(full_ells, ells_theory, Cls_unbinned)
 
-            # for mock data, the data ells are the binned theory ells, so there's already perfect syncronization with the theory Cl's
-            theoryvector_binned.append( yalm.binTheory(interp_Cls_unbinned, self.wsp_name) )
+            #Confirm Mode-Coupling Matrix 
+            if freq_list[iCl] not in self.mcms_paths_list[iCl]:
+                raise ValueError(f"Your mcms are in the wrong order, beginning with '{self.mcms_paths_list[iCl]}'. They must be in order of ascending frequency.")
 
-        # theoryvector_binned = theoryvector_unbinned
+            #Bin Theory Vector
+            # for mock data, the data ells are the binned theory ells, so there's already perfect syncronization with the theory Cl's
+            theoryvector_binned.append( yalm.binTheory(interp_Cls_unbinned, self.mcms_paths_list[iCl]) )
 
         #Create large, Multifreq Theory Vector
         theoryvector = np.array(theoryvector_binned).flatten()
 
         #Debugging
-        # debugfname = self.data_directory + 'mcmcresults/steps/' + f'steps_Cls_{self.rank}_{self.debug_index}.npy'
-        # # if not os.path.isfile(debugfname):
-        # #     np.savetxt(debugfname, [theoryvector])
-        # # else:
-        # #     # import pdb; pdb.set_trace()
-        # #     all_theoryCls = np.loadtxt(debugfname)
-        # #     if all_theoryCls.ndim == 1:
-        # #         np.savetxt(debugfname, np.stack( (all_theoryCls, theoryvector) ))
-        # #     elif all_theoryCls.ndim == 2: 
-        # #         np.savetxt(debugfname, np.concatenate( (all_theoryCls, theoryvector[None, :]) ))
-        # #     else:
-        # #         raise ValueError('something has gone horribly wrong with saving the previous spectra!')
-        # np.save(debugfname, theoryvector)
-        # self.debug_index += 1
+        debugfname = self.data_directory + 'mcmcresults/steps/' + f'steps_Cls_{self.rank}_{self.debug_index}.npy'
+        # if not os.path.isfile(debugfname):
+        #     np.savetxt(debugfname, [theoryvector])
+        # else:
+        #     # import pdb; pdb.set_trace()
+        #     all_theoryCls = np.loadtxt(debugfname)
+        #     if all_theoryCls.ndim == 1:
+        #         np.savetxt(debugfname, np.stack( (all_theoryCls, theoryvector) ))
+        #     elif all_theoryCls.ndim == 2: 
+        #         np.savetxt(debugfname, np.concatenate( (all_theoryCls, theoryvector[None, :]) ))
+        #     else:
+        #         raise ValueError('something has gone horribly wrong with saving the previous spectra!')
+        np.save(debugfname, theoryvector)
+        self.debug_index += 1
 
         return theoryvector
