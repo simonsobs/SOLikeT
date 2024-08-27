f78cf98f91d3fc3c582410d2c7da8dab1e3a890e
Giacomo Galloni <giacomo.galloni@unife.it>
2024-08-27 10:01:50 +0200

    Add mode specific imports

--- soliket/tests/test_mflike.py
+++ soliket/tests/test_mflike.py
@@ -1,3 +1,7 @@
 def test_import():
+
     import mflike  # noqa: F401
-    from mflike import BandpowerForeground, Foreground, MFLike # noqa: F401
+
+    from mflike import BandpowerForeground, Foreground # noqa: F401
+
+    from mflike import TTTEEE, TT, EE, TE # noqa: F401
