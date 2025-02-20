45efed709c3ac18ce2cd87fb0f1468a9df3dd815
Giacomo Galloni <giacomo.galloni@unife.it>
2025-02-20 16:48:07 +0100

    Add data installation test for 'mflike'

--- soliket/tests/test_mflike.py
+++ soliket/tests/test_mflike.py
@@ -1,3 +1,8 @@
+from cobaya.tools import resolve_packages_path
+
+packages_path = resolve_packages_path()
+
+
 def test_import():
 
     import mflike  # noqa: F401
@@ -5,3 +10,22 @@ def test_import():
     from mflike import BandpowerForeground, Foreground # noqa: F401
 
     from mflike import TTTEEE, TT, EE, TE # noqa: F401
+
+
+def test_install(request):
+    from cobaya.install import install
+
+    mflike_options = {
+        "input_file": "LAT_simu_sacc_00044.fits",
+        "cov_Bbl_file": "data_sacc_w_covar_and_Bbl.fits",
+        "stop_at_error": True,
+    }
+
+    install(
+        {"likelihood": {"mflike.TTTEEE": mflike_options}},
+        path=packages_path,
+        skip_global=False,
+        force=False,
+        debug=True,
+        no_set_global=True,
+    )
