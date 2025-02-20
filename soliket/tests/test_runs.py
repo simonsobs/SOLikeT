a5a687fab607d5e2c63844a112cacb0f3494af43
Giacomo Galloni <giacomo.galloni@unife.it>
2025-02-20 16:49:15 +0100

    Ensure data are downloaded before running tests on them

--- soliket/tests/test_runs.py
+++ soliket/tests/test_runs.py
@@ -1,11 +1,40 @@
 import pkgutil
 
 import pytest
+from cobaya.install import install
 from cobaya.run import run
 from cobaya.tools import resolve_packages_path
 from cobaya.yaml import yaml_load
 
 packages_path = resolve_packages_path()
+
+
+@pytest.mark.parametrize("lhood",
+                         ["lensing",
+                          "multi",
+                          ])
+def test_installation(lhood):
+    if lhood == "lensing":
+        from soliket import LensingLikelihood
+
+        is_installed = LensingLikelihood.is_installed(
+            path=packages_path,
+        )
+        assert is_installed is True, (
+            "LensingLikelihood is not installed! Please install it using "
+            "'cobaya-install soliket.LensingLikelihood'"
+        )
+
+    elif lhood == "multi":
+        import mflike
+
+        is_installed = mflike.TTTEEE.is_installed(
+            path=packages_path,
+        )
+        assert is_installed is True, (
+            "mflike.TTTEEE is not installed! Please install it using "
+            "'cobaya-install mflike.TTTEEE'"
+        )
 
 
 @pytest.mark.parametrize("lhood",
@@ -21,7 +50,6 @@ def test_evaluate(lhood):
     info["force"] = True
     info['sampler'] = {'evaluate': {}}
 
-    from cobaya.install import install
     install(info, path=packages_path, skip_global=True, no_set_global=True)
 
     updated_info, sampler = run(info)
@@ -40,7 +68,6 @@ def test_mcmc(lhood):
     info["force"] = True
     info['sampler'] = {'mcmc': {'max_samples': 10, 'max_tries': 1000}}
 
-    from cobaya.install import install
     install(info, path=packages_path, skip_global=True, no_set_global=True)
 
     updated_info, sampler = run(info)
