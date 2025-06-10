5f530fa6f53e4b8a86dd00af84a90bcf55d8e1a4
Giacomo Galloni <giacomo.galloni@unife.it>
2025-06-10 21:21:41 +0200

    Avoid `MFLike` on `py3.13`

--- ci_scripts/install_likelihoods.sh
+++ ci_scripts/install_likelihoods.sh
@@ -6,9 +6,18 @@ OS="$(uname | tr '[:upper:]' '[:lower:]')"
 
 echo "Running install_likelihoods.sh on $OS"
 
-# Install MFLike
-echo "Installing MFLike likelihood..."
-uv run cobaya-install mflike.TTTEEE --no-set-global
+# Detect Python major/minor
+PY_MAJOR=$(python -c 'import sys; print(sys.version_info.major)')
+PY_MINOR=$(python -c 'import sys; print(sys.version_info.minor)')
+echo "Detected Python version: ${PY_MAJOR}.${PY_MINOR}"
+
+# Install MFLike only on Python < 3.13
+if [[ $PY_MAJOR -eq 3 && $PY_MINOR -lt 13 ]]; then
+  echo "Installing MFLike likelihood..."
+  uv run cobaya-install mflike.TTTEEE --no-set-global
+else
+  echo "Skipping MFLike on Python ${PY_MAJOR}.${PY_MINOR} (requires <3.13)"
+fi
 
 # Install Planck likelihood only if not on Windows
 if [[ "$OS" != "mingw"* && "$OS" != "cygwin"* && "$OS" != "msys"* ]]; then
